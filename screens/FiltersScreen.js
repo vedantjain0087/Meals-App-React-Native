cf78d3a880b5c3f6febe0e889f6b64a8024dab73
Vedant <mainvedant.vns123@gmail.com>
2019-09-28 16:47:14 +0530

    first commit

---
+++ screens/FiltersScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+const FilterScreen = props =>{
+return (
+    <View style={styles.screen}>
+        <Text>The FilterScreen</Text>
+    </View>
+)
+}
+const styles = StyleSheet.create({
+screen:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center'
+}
+});
+export default FilterScreen;
