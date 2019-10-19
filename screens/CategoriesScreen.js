0c6062a79c6d984727635d559fbf859e7df3aa93
Vedant <mainvedant.vns123@gmail.com>
2019-10-19 23:34:07 +0530

    header and grid

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,42 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button, TouchableOpacity, Platform} from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import {CATEGORIES} from '../data/dummy-data';
+import Colors from '../constants/Colors';
+
+
 const CategoriesScreen = props =>{
+const renderGridItem = (itemData) =>{
+    return <TouchableOpacity style={styles.gridItem} onPress={()=>{
+        props.navigation.navigate({routeName:"CategoryMeals"})
+    }}>
+        <View>
+            <Text>{itemData.item.title}</Text>
+        </View>
+        </TouchableOpacity>;
+}
 return (
-    <View style={styles.screen}>
-        <Text>The Categories Screen</Text>
-    </View>
+ <FlatList keyExtractor={(item, index) => item.id} data={CATEGORIES } renderItem={renderGridItem} numColumns={2}/>
 )
+}
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle:{
+        backgroundColor: Platform.OS == 'android'? Colors.primaryColor :'white',
+    },
+    headerTintColor:Platform.OS == 'android'? "white": Colors.primaryColor
 }
 const styles = StyleSheet.create({
 screen:{
     flex:1,
     justifyContent:'center',
     alignItems:'center'
+},
+gridItem:{
+    flex:1,
+    margin:15,
+    height:150
 }
 });
 export default CategoriesScreen;
