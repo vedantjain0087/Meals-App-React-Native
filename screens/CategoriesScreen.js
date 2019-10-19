e22004929f984acc9576a45789a9a83ef2f0e89e
Vedant <mainvedant.vns123@gmail.com>
2019-10-20 00:15:46 +0530

    params

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -8,7 +8,9 @@ import Colors from '../constants/Colors';
 const CategoriesScreen = props =>{
 const renderGridItem = (itemData) =>{
     return <TouchableOpacity style={styles.gridItem} onPress={()=>{
-        props.navigation.navigate({routeName:"CategoryMeals"})
+        props.navigation.navigate({routeName:"CategoryMeals", params:{
+            categoryId: itemData.item.id
+        }})
     }}>
         <View>
             <Text>{itemData.item.title}</Text>
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {CATEGORIES} from '../data/dummy-data';
+
 const CategoryMealsScreen = props =>{
+const catId = props.navigation.getParam('categoryId');
+const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
 return (
     <View style={styles.screen}>
         <Text>The CategoryMealsScreen</Text>
+        <Text>{selectedCategory.title}</Text>
     </View>
 )
 }
