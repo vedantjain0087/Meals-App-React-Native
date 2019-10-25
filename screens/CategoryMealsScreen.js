65ccf9f4bf3435918db28f0e7f1c7fe0af52a205
Vedant <mainvedant.vns123@gmail.com>
2019-10-25 18:06:11 +0530

    meal details cards

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import {View, Text, StyleSheet, Platform} from 'react-native';
-import {CATEGORIES} from '../data/dummy-data';
+import {View, Text, FlatList, StyleSheet, Platform} from 'react-native';
+import {CATEGORIES, MEALS} from '../data/dummy-data';
 import Colors from '../constants/Colors';
-
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = props =>{
+const renderMealItem = itemData => {
+    return (
+        <MealItem title={itemData.item.title}
+         duration={itemData.item.duration}
+         image={itemData.item.imageUrl}
+         complexity={itemData.item.complexity}
+         affordability={itemData.item.affordability}
+          onSelectMeal={() => {}}/>
+    );
+}    
 const catId = props.navigation.getParam('categoryId');
-const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+// const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0)
 
 return (
     <View style={styles.screen}>
-        <Text>The CategoryMealsScreen</Text>
-        <Text>{selectedCategory.title}</Text>
+        <FlatList style={{width:'95%'}}data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem}/>
     </View>
 )
 }
