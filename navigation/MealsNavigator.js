4c1ca264a92f4234c690d184b5f009e1eb51a5a4
Vedant <mainvedant.vns123@gmail.com>
2019-10-26 18:51:41 +0530

    bottom tab navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,11 +1,16 @@
-import { createAppContainer } from 'react-navigation';
+import React from 'react';
+import { createAppContainer, } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import {Platform} from 'react-native';
+import {Ionicons} from '@expo/vector-icons'
+
 import Colors from '../constants/Colors';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoriteScreen from '../screens/FavoriteScreen';
 
 const MealsNavigator = createStackNavigator({
     Categories:{
@@ -25,4 +30,25 @@ const MealsNavigator = createStackNavigator({
     }
 })
 
-export default createAppContainer(MealsNavigator)
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: { screen: MealsNavigator,
+        navigationOptions:{
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor}/>
+            }
+        }
+    },
+    Favorites: {screen: FavoriteScreen,
+        navigationOptions:{
+            tabBarIcon: (tabInfo) => {
+                return <Ionicons name="ios-star" size={25} color={tabInfo.tintColor}/>
+            }
+        }
+    }
+}, {
+    tabBarOptions:{
+        activeTintColor: Colors.accentColor
+    }
+})
+
+export default createAppContainer(MealsFavTabNavigator)
