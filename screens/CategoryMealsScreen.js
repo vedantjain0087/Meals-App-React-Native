c62065a870bee8846e355e906b739cb4de70e8c0
Vedant <mainvedant.vns123@gmail.com>
2019-10-26 18:14:28 +0530

    star icon

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -12,7 +12,11 @@ const renderMealItem = itemData => {
          image={itemData.item.imageUrl}
          complexity={itemData.item.complexity}
          affordability={itemData.item.affordability}
-          onSelectMeal={() => {}}/>
+          onSelectMeal={() => {
+              props.navigation.navigate({routeName:'MealDetail', params:{
+                  mealId:itemData.item.id
+              }})
+          }}/>
     );
 }    
 const catId = props.navigation.getParam('categoryId');
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+
+import {MEALS} from '../data/dummy-data';
+import CustomHeaderButton from '../components/HeaderButton';
+
 const MealDetailScreen = props =>{
+    const mealId = props.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId)
 return (
     <View style={styles.screen}>
-        <Text>The MealDetailScreen</Text>
+        <Text>{selectedMeal.title}</Text>
     </View>
 )
 }
@@ -14,4 +21,21 @@ screen:{
     alignItems:'center'
 }
 });
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+    const mealId = navigationData.navigation.getParam('mealId');
+    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    return {
+        headerTitle:selectedMeal.title,
+        headerRight:(
+        <HeaderButtons HeaderButtonComponent={CustomHeaderButton}> 
+            <Item title='Favorite' iconName='ios-star' onPress={() => {
+                console.log("Marked")
+            }}/>
+        </HeaderButtons>
+        )
+    }
+
+
+}
 export default MealDetailScreen;
