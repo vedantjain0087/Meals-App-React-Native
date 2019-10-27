96a3202c14e6b32543011225993e820d5f6691b6
Vedant <mainvedant.vns123@gmail.com>
2019-10-27 16:43:15 +0530

    filters

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,21 +1,54 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, Switch} from 'react-native';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons'
 
 import CustomHeaderButton from '../components/HeaderButton';
+import Colors from '../constants/Colors';
 
+const FilterSwitch = props =>{
+    return (
+        <View style={styles.filterContainer}>
+        <Text>{props.label}</Text>
+        <Switch 
+        trackColor={{true: Colors.primaryColor}}
+        value={props.state} 
+        onValueChange={props.onChange}/>
+    </View>
+    )
+}
 const FilterScreen = props =>{
+    const [isGlutenFree, setIsGlutenFree] = useState(false);
+    const [isLactoseFree, setIsLactoseFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegeterian, setIsVegeterian] = useState(false);
+
 return (
     <View style={styles.screen}>
-        <Text>The FilterScreen</Text>
+        <Text style={styles.title}>Avaialable Filters / Restrictions</Text>
+        <FilterSwitch label='Gluten-free' state={isGlutenFree} onChange={newValue => setIsGlutenFree(newValue)}/>
+        <FilterSwitch label='Lactose-free' state={isLactoseFree} onChange={newValue => setIsLactoseFree(newValue)}/>
+        <FilterSwitch label='Vegan' state={isVegan} onChange={newValue => setIsVegan(newValue)}/>
+        <FilterSwitch label='Vegeterian' state={isVegeterian} onChange={newValue => setIsVegeterian(newValue)}/>
     </View>
 )
 }
 const styles = StyleSheet.create({
 screen:{
     flex:1,
-    justifyContent:'center',
     alignItems:'center'
+},
+title:{
+    fontFamily:'open-sans-bold',
+    fontSize:22,
+    margin:20,
+    textAlign:'center'
+},
+filterContainer:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    width:'80%',
+    marginVertical:15
 }
 });
 
