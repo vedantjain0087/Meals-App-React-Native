7d564c80f4a13fac163d6f3f2e4ab1cf3b91f7b5
Vedant <mainvedant.vns123@gmail.com>
2019-10-29 14:05:08 +0530

    redux favorite

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,9 +1,10 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useCallback} from 'react';
 import {ScrollView,View, Image, Text, StyleSheet} from 'react-native';
 import {HeaderButtons, Item} from 'react-navigation-header-buttons';
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 
 import CustomHeaderButton from '../components/HeaderButton';
+import {toggleFavorite} from '../store/actions/meals'
 
 const ListItem = props =>{
     return (
@@ -18,10 +19,14 @@ const MealDetailScreen = props =>{
     const mealId = props.navigation.getParam('mealId');
     const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
-    // useEffect(() => {
-    //     props.navigation.setParams({mealTitle: selectedMeal.title})
-    // }, [selectedMeal])
-    
+    const dispatch = useDispatch();
+    const toggleFavoriteHandler = useCallback(() => {
+        dispatch(toggleFavorite(mealId))
+    }, [dispatch, mealId]);
+    useEffect(() => {
+        props.navigation.setParams({toggleFav: toggleFavoriteHandler})
+    }, [toggleFavoriteHandler])
+
 return (
     <ScrollView>
         <Image source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
@@ -64,13 +69,12 @@ listItem:{
 MealDetailScreen.navigationOptions = (navigationData) => {
     const mealId = navigationData.navigation.getParam('mealId');
     const mealTitle = navigationData.navigation.getParam('mealTitle');
+    const toggleFavorite = navigationData.navigation.getParam('toggleFav')
     return {
         headerTitle:mealTitle,
         headerRight:(
         <HeaderButtons HeaderButtonComponent={CustomHeaderButton}> 
-            <Item title='Favorite' iconName='ios-star' onPress={() => {
-                console.log("Marked")
-            }}/>
+            <Item title='Favorite' iconName='ios-star' onPress={toggleFavorite}/>
         </HeaderButtons>
         )
     }
--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,4 +1,6 @@
 import {MEALS} from '../../data/dummy-data';
+import {TOGGLE_FAVORITE} from '../actions/meals';
+
 const initialState = {
     meals:MEALS,
     filteredMeals:MEALS,
@@ -6,7 +8,21 @@ const initialState = {
 }
 
 const mealsReducer = (state = initialState, action) => {
-    return state
+    switch(action.type){
+        case TOGGLE_FAVORITE:
+            const existingIndex = state.favoriteMeals.findIndex(meal => meal.id === action.mealId);
+            if(existingIndex >= 0){
+                const updatedFavMeals = [...state.favoriteMeals];
+                updatedFavMeals.splice(existingIndex,1)
+                return {...state, favoriteMeals:updatedFavMeals}
+            } else{
+                return {
+                    ...state, favoriteMeals: state.favoriteMeals.concat(state.meals.find(meal => meal.id === action.mealId))
+                }
+            }  
+        default:
+            return state    
+    }
 }
 
 export default mealsReducer
